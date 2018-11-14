3db81d00dd3911e950954b193f3c96292e79dd29
Dongyang Li <donnie.ldyang@gmail.com>
2018-11-14 00:15:23 -0800

    go

--- randomproblems/partitionList3Parts.cpp
+++ randomproblems/partitionList3Parts.cpp
@@ -68,7 +68,7 @@ void partitionList(std::vector<int>& nums, int x){
     */
 
     //more effecient approach
-    //just use left and right
+    //only one loop
     int firstEnd=0; //it could be end of first partition or start of second partition(first 10)
     int thirdStart=0;
     bool thirdStartExist = false;
@@ -79,31 +79,31 @@ void partitionList(std::vector<int>& nums, int x){
                 firstEnd = i;
             }
             if(nums[firstEnd] == x){
-                std::swap(nums[firstEnd], nums[i]);
-                if(i == nums.size()-1){
+                std::swap(nums[firstEnd], nums[i]); //swap smaller element with first x
+                if(i == nums.size()-1){// return if it is the last element, otherwise {10,10,2} will fail by output:{10,2,10}
                     return;
                 }
-                if(nums[firstEnd+1] == x){
+                if(nums[firstEnd+1] == x){ //if next one is x, then move firstEnd to the first x
                     firstEnd++;
                 }
             }
         }
 
-        if(nums[i] > x && thirdStartExist == false){
+        if(nums[i] > x && thirdStartExist == false){ //if thirdStart havent been set, in case the first element is thirdStart
             thirdStart = i;
             thirdStartExist = true;
         }
 
-        if(firstEnd > thirdStart){
+        if(firstEnd > thirdStart){ //only happens when there is no x in front of thirdStart, and a new smaller found
             std::swap(nums[firstEnd], nums[thirdStart]);
             int temp = firstEnd;
             firstEnd = thirdStart;
             thirdStart++;
         }
 
-        if(nums[thirdStart] > x && nums[i] == x){
+        if(nums[thirdStart] > x && nums[i] == x){ //make sure thirdStart is not at the initial 0 state
             std::swap(nums[thirdStart], nums[i]);
-            if(nums[thirdStart-1] != x){
+            if(nums[thirdStart-1] != x){ // mark the first x as the firstEnd (for later swapping smaller element with x)
                 firstEnd = thirdStart;
             }
             thirdStart++;
@@ -113,11 +113,11 @@ void partitionList(std::vector<int>& nums, int x){
 }
 
 int main(){
-    std::vector<int> nums = {9, 14, 10};
+    //std::vector<int> nums = {9, 14, 10};
     //std::vector<int> nums = {14, 10};
     //std::vector<int> nums = {15, 9, 14, 10, 5, 10, 2, 10, 10, 10, 10};
     //std::vector<int> nums = {15, 9, 14, 2, 5};
-    //std::vector<int> nums = {15, 10, 10, 10};
+    std::vector<int> nums = {10, 10, 15, 2, 10};
     partitionList(nums, 10);
 
     for(int i=0; i<nums.size(); i++){
